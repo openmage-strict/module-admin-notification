9c773339e93f1574f11b271562bf71d3f719ad2b
Sven Reichel <github-sr@hotmail.com>
2023-12-10 04:29:57 +0100

    PHPMD updates
    - closes #4

--- src/app/code/core/Mage/AdminNotification/Helper/Data.php
+++ src/app/code/core/Mage/AdminNotification/Helper/Data.php
@@ -70,6 +70,9 @@ class Mage_AdminNotification_Helper_Data extends Mage_Core_Helper_Abstract
      * @param bool $withExt
      * @return string
      * @deprecated v19.4.16
+     *
+     * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function getPopupObjectUrl($withExt = false)
     {
